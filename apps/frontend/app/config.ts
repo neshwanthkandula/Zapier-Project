54085e8a2f940b6acf049095a85c85018dc4c18a
neshwanthkandula <kandulaneshwanth@gmail.com>
2025-09-14 13:23:25 +0530

    Fix URL

--- apps/frontend/app/config.ts
+++ apps/frontend/app/config.ts
@@ -1,9 +1,9 @@
 
 // lib/config.js
-export const VM_IP = process.env.NEXT_PUBLIC_VM_IP;
+export const VM_IP = process.env.NEXT_PUBLIC_VM_IP ;
 export const BACKEND_PORT = process.env.NEXT_PUBLIC_BACKEND_PORT;
 export const HOOK_PORT = process.env.NEXT_PUBLIC_HOOK_PORT;
 
 // Construct URLs using the environment variables
-export const BACKEND_URL = `http://${VM_IP}:${BACKEND_PORT}/api/v1` || "http://localhost:3004/api/v1";
-export const HOOK_URL = `http://${VM_IP}:${HOOK_PORT}/hooks/catch/` || "http://localhost:3003/hooks/catch/";
+export const BACKEND_URL = `http://${VM_IP}:${BACKEND_PORT}/api/v1`
+export const HOOK_URL = `http://${VM_IP}:${HOOK_PORT}/hooks/catch/`
